af917a06567b6409a16815a4439f29cad8b10382
ikerbedmar <100535185+ikerbedmar@users.noreply.github.com>
2022-04-04 00:10:02 +0200

    Update menu_controller.js

--- pi_3/js/menu_controller.js
+++ pi_3/js/menu_controller.js
@@ -1,18 +1,10 @@
 function start_game(){
 	name = prompt("User name");
-	
-	sessionStorage.setItem("username", name);
-	
 	loadpage("./html/game.html");
 }
 
 function exit (){
-	if (name != ""){
-		alert("Leaving " + name + "'s game");
-	}
-	name = "";
-}
-
+	loadpage("../index.html");
 function options(){
 	loadpage("./html/options.html");
 }
